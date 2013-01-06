918060e2fc0dfa29b2f9cafa65ea9bc13454f6d2
triley63 <rileyta63@yahoo.com>
2013-01-06 10:32:10 -0600

    Project Initialization

---
+++ VPS.Common.Data.UI.ConsoleApp.Scrubber/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VPS.Common.Data.UI.ConsoleApp.Scrubber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
